e36b8820402399c888ab476f527a1f20bcee934f
Nathan Levesque <rhysyngsun@gmail.com>
2023-02-15 16:00:53 -0500

    Fix setup.py to not use lib3to6

--- setup.py
+++ setup.py
@@ -52,12 +52,6 @@ classifiers = [
 
 package_dir = {"": "src"}
 
-
-if any(arg.startswith("bdist") for arg in sys.argv):
-    import lib3to6
-    package_dir = lib3to6.fix(package_dir)
-
-
 setuptools.setup(
     name="lexid",
     license="MIT",
